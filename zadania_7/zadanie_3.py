7e25b882d50e2177da2c103e53d4c29e75f73af4
Adrian096 <36916073+Adrian096@users.noreply.github.com>
2018-04-22 13:45:07 +0200

    Update zadanie_3.py

--- zadania_7/zadanie_3.py
+++ zadania_7/zadanie_3.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.types import NUMERIC
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,7 +9,7 @@ Base = declarative_base()
 
 class Studenci(Base):
     __tablename__ = 'studenci'
-    id = Column(Integer, primary_key = True, autoincrement = True)
+    id = Column(Integer, primary_key = True)
     name = Column(String(100))
     surname = Column(String(100))
     book_id = Column(NUMERIC)
@@ -16,7 +17,7 @@ class Studenci(Base):
 
 class Ksiazki(Base):
     __tablename__ = 'ksiazki'
-    id = Column(Integer, primary_key = True, autoincrement = True)
+    id = Column(Integer, primary_key = True)
     title = Column(String(100))
     publishing = Column(String(100))
     student_id = Column(NUMERIC)
@@ -29,117 +30,28 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+for ksiazki in session.query(Ksiazki).all():
+    print(ksiazki.id, ksiazki.title, ksiazki.publishing, ksiazki.student_id)
 
-def wyswietlTabele(ch=0):
-    print("Wybierz tabele:")
-    print("1.Ksiazki")
-    print("2.Studenci")
-    if(ch == 0):
-        choose = int(input("Wybierz: "))
-    else:
-        choose = ch
+for studenci in session.query(Studenci).all():
+    print(studenci.id, studenci.name, studenci.surname, studenci.book_id)
 
-    if (choose == 1):
-        for ksiazki in session.query(Ksiazki).all():
-            print(ksiazki.id, ksiazki.title, ksiazki.publishing, ksiazki.student_id)
-    elif (choose == 2):
-        for studenci in session.query(Studenci).all():
-            print(studenci.id, studenci.name, studenci.surname, studenci.book_id)
+new_data = Ksiazki(
+    id = 3,
+    title=u'Metro 2033',
+    publishing=u'Irsing',
+    student_id=1
+)
 
-def dodajRekord():
-    print("Wybierz tabele:")
-    print("1.Ksiazki")
-    print("2.Studenci")
-
-    choose = int(input("Wybierz: "))
-
-    if(choose == 1):
-        title = input("Tytuł: ")
-        publishing = input("Wydawca: ")
-        new_data = Ksiazki(
-            title = title,
-            publishing = publishing
-        )
-        session.add(new_data)
-        session.commit()
-    elif(choose == 2):
-        name = input("Imie: ")
-        surname = input("Nazwisko: ")
-        new_data = Studenci(
-            name = name,
-            surname = surname
-        )
-        session.add(new_data)
-        session.commit()
+new_data2 = Studenci(
+    id = 1,
+    name=u'Jan',
+    surname=u'Kowalski',
+    book_id=5
+)
 
 
-def usunRekord():
-    print("Wybierz tabele:")
-    print("1.Ksiazki")
-    print("2.Studenci")
 
-    choose = int(input("Wybierz: "))
-    if (choose == 1):
-        print("Wyswietlic tabele?")
-        print("1.TAK")
-        print("2.NIE")
-        w = int(input("TAK-1/NIE-2: "))
-        if (w == 1):
-            wyswietlTabele(1)
-
-        idDel = int(input("Podaj id: "))
-
-        obj = session.query(Ksiazki).filter_by(id=idDel).one()
-        session.delete(obj)
-        session.commit()
-    elif(choose == 2):
-        print("Wyswietlic tabele?")
-        print("1.TAK")
-        print("2.NIE")
-        w = int(input("TAK-1/NIE-2: "))
-        if (w == 1):
-            wyswietlTabele(2)
-
-        idDel = int(input("Podaj id: "))
-        obj = session.query(Studenci).filter_by(id=idDel).one()
-        session.delete(obj)
-        session.commit()
-
-
-def wypozyczona():
-    print("Wyswietlic tabele?")
-    print("1.TAK")
-    print("2.NIE")
-    w = int(input("TAK-1/NIE-2: "))
-    if (w == 1):
-        wyswietlTabele(1)
-        wyswietlTabele(2)
-    idK = int(input("Podaj id książki"))
-    idS = int(input("Podaj id studenta"))
-
-    ksiazka = session.query(Ksiazki).filter_by(id=idK).one()
-    student = session.query(Studenci).filter_by(id=idS).one()
-    ksiazka.student_id = idS
-    student.book_id = idK
-    session.commit()
-
-choose = 0
-
-while(choose != 9):
-    print("Wybierz działanie")
-    print("1.dodaj rekord do bazy danych")
-    print("2.usuń rekord z bazy danych")
-    print("3.wyswietl tabele")
-    print("4.ustaw wypozyczenie")
-    choose = int(input("działanie: "))
-
-    if(choose == 1):
-        dodajRekord()
-    elif(choose == 2):
-        usunRekord()
-    elif(choose == 3):
-        wyswietlTabele()
-    elif(choose == 4):
-        wypozyczona()
-
-
+session.add(new_data)
+session.add(new_data2)
+session.commit()
