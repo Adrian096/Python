af115544af867ba9e141518b98d525d6e97b00a1
Adrian096 <36916073+Adrian096@users.noreply.github.com>
2018-05-09 11:43:05 +0200

    Add files via upload

--- Zadania_9/zadanie_2.py
+++ Zadania_9/zadanie_2.py
@@ -1,5 +1,6 @@
 import Tkinter as tk
 import tkFont as tkF
+import time as tm
 
 root = tk.Tk()
 
@@ -7,6 +8,10 @@ default_font = tkF.nametofont("TkDefaultFont")
 default_font.configure(size=16)
 root.option_add("*Font", default_font)
 
+i = 20
 
+for i in range(0, 20):
+    i = i+1
+    tm.sleep(1000)
+    print(i)
 
-root.mainloop()
